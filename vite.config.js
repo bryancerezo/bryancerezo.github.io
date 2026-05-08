10f57b5f35c4201c0e36f3ce6793340414d391b1
Bryan <5120044+bryancerezo@users.noreply.github.com>
2026-05-08 08:53:20 +0000

    v9

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [react()],
   // If your site is at username.github.io/repo-name/, 
   // set base to '/repo-name/'. If using a custom domain, use '/'
-  base: '/https://bryancerezo.github.io/', 
+  base: '/bryancerezo.github.io/', 
 })
