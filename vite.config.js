4a92d8a66411bb4ac536a1da40cf268a1a4028c8
Bryan <5120044+bryancerezo@users.noreply.github.com>
2026-05-08 15:59:22 +0800

    Modified index.html and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // If your site is at username.github.io/repo-name/, 
+  // set base to '/repo-name/'. If using a custom domain, use '/'
   base: './', 
 })
