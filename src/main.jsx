d3c0ced021019c0a4504713a2a8b278efc3ec4d2
Bryan <5120044+bryancerezo@users.noreply.github.com>
2026-05-08 08:33:19 +0000

    v6

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
 // Inline styles for base layout normalization
 const style = document.createElement('style');
